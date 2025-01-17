2fb32612191b1b65ffa859049e0aabb67442c6fa
Adel Moumen <adelmoumen.pro@gmail.com>
2025-01-17 10:07:34 -0500

    add init.py / modify paths

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='kokoro',  # Name of the package
-    version='0.1.0',           # Initial version
+    version='0.1.0',  # Initial version
     packages=find_packages(),  # Automatically finds packages
-    install_requires=[         # List your dependencies here
+    install_requires=[  # List your dependencies here
         'numpy',
         'phonemizer',
         'scipy',
@@ -15,7 +19,7 @@ setup(
     author='hexgrad',
     author_email='hello@hexgrad.com',
     description='TTS',
-    long_description=open('README.md').read(),  # Content from your README
+    long_description=long_description,  # Content from your README
     long_description_content_type='text/markdown',  # Required for markdown
     url='https://github.com/hexgrad/kokoro',  # GitHub repo URL
     license='Apache 2.0',
