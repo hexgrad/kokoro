209a63b91f12cea7ee8e261cd56e511fd0f90c00
hexgrad <hello@hexgrad.com>
2025-01-27 15:42:27 -0800

    misaki[en]>=0.6.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='kokoro',  # Name of the package
-    version='0.1.1',           # Initial version
+    version='0.1.2',           # Initial version
     packages=find_packages(),  # Automatically finds packages
     install_requires=[         # List your dependencies here
         'huggingface_hub',
-        'misaki[en]>=0.5.9',
+        'misaki[en]>=0.6.0',
         'numpy',
         'scipy',
         'torch',
