11743daf8d0e96a591c32cb7efb86a8375681ff6
fondoger <fondoger@outlook.com>
2025-04-03 19:00:05 +0800

    Fix

--- kokoro/pipeline.py
+++ kokoro/pipeline.py
@@ -94,14 +94,14 @@ class KPipeline:
         elif model:
             if device == 'cuda' and not torch.cuda.is_available():
                 raise RuntimeError("CUDA requested but not available")
-            if device is 'mps' and not torch.backends.mps.is_available():
+            if device == 'mps' and not torch.backends.mps.is_available():
                 raise RuntimeError("MPS requested but not available")
-            if device is 'mps' and os.environ.get('PYTORCH_ENABLE_MPS_FALLBACK') != 1:
+            if device == 'mps' and os.environ.get('PYTORCH_ENABLE_MPS_FALLBACK') != '1':
                 raise RuntimeError("MPS requested but fallback not enabled")
             if device is None:
                 if torch.cuda.is_available():
                     device = 'cuda'
-                elif os.environ.get('PYTORCH_ENABLE_MPS_FALLBACK') == 1 and torch.backends.mps.is_available():
+                elif os.environ.get('PYTORCH_ENABLE_MPS_FALLBACK') == '1' and torch.backends.mps.is_available():
                     device = 'mps'
                 else:
                     device = 'cpu'
