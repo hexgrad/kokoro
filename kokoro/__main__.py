c654bff974b1bd0f93487a7a58d6590a73b858b3
Aleix Conchillo Flaqué <aleix@daily.co>
2026-01-30 16:03:58 -0800

    move loguru setup from __init__ to __main__
    This allows other applications that also use Kokoro to setup loguru properly.

--- kokoro/__main__.py
+++ kokoro/__main__.py
@@ -13,12 +13,29 @@ espeak not installed: `apt-get install espeak-ng`
 """
 
 import argparse
+import sys
 import wave
 from pathlib import Path
 from typing import Generator, TYPE_CHECKING
 
 import numpy as np
 from loguru import logger
+
+# Remove default handler
+logger.remove()
+
+# Add custom handler with clean format including module and line number
+logger.add(
+    sys.stderr,
+    format="<green>{time:HH:mm:ss}</green> | <cyan>{module:>16}:{line}</cyan> | <level>{level: >8}</level> | <level>{message}</level>",
+    colorize=True,
+    level="INFO" # "DEBUG" to enable logger.debug("message") and up prints
+                 # "ERROR" to enable only logger.error("message") prints
+                 # etc
+)
+
+# Disable before release or as needed
+logger.disable("kokoro")
 
 languages = [
     "a",  # American English
